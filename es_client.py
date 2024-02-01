7b6c31c41ce1657afeac4599d714bc15b5367e84
jalaine329 <100935722+jalaine329@users.noreply.github.com>
2024-02-01 14:21:32 +0100

    Create es_client.py

---
+++ es_client.py
@@ -0,0 +1,23 @@
+from elasticsearch import Elasticsearch
+
+# prompt the user to choose a cluster and validate the input
+def select_cluster(clusters):
+    print("Available Clusters:")
+    cluster_names = list(clusters.keys())
+    for idx, cluster_name in enumerate(cluster_names, start=1):
+        print(f"{idx}. {cluster_name}")
+    while True:
+        try:
+            selected_cluster = int(input("Enter the number of the cluster you want to use: ")) - 1
+            if 0 <= selected_cluster < len(cluster_names):
+                return cluster_names[selected_cluster], clusters[cluster_names[selected_cluster]]
+            else:
+                print("Invalid selection. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+# initialize elasticsearch client w/ the selected cluster and authentication
+def init_es_client(cluster_urls):
+    username = input("Enter your Elasticsearch username: ")
+    password = input("Enter your Elasticsearch password: ")
+    return Elasticsearch(cluster_urls, http_auth=(username, password), verify_certs=False, timeout=30)
